bc1bd003ca74947b44bd62cacab49a483db404fb
iCarlSO <91926524+icarlso8@users.noreply.github.com>
2025-10-23 12:10:32 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# TrkknCol
+
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAA7SZYGbCZrBpurLVrW4RPbM5Zl-32Mog",
+  authDomain: "trkkn-cot-firebase.firebaseapp.com",
+  projectId: "trkkn-cot-firebase",
+  storageBucket: "trkkn-cot-firebase.appspot.com",
+  messagingSenderId: "490397786735",
+  appId: "1:490397786735:web:6cc8d158f904865bdc239a"  
+};
